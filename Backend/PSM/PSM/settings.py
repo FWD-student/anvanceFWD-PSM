15c50a0ebcf95402f42f608557c0549d714a58ca
Glumie <luisblocon15@gmail.com>
2026-02-13 02:51:28 -0600

    correccion de ssl

--- Backend/PSM/PSM/settings.py
+++ Backend/PSM/PSM/settings.py
@@ -222,6 +222,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SEGURIDAD EN PRODUCCIÓN
 # =============================================================================
 if not DEBUG:
+    # Confiar en el header de Railway para saber si estamos en HTTPS
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
